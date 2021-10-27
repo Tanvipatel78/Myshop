69a6c49866c3f480e49fc2f38f2ead8bc4877a3f
Tanvipatel78 <80584956+Tanvipatel78@users.noreply.github.com>
2021-10-27 15:57:06 +0530

    Entityframework

---
+++ myShop/MyshopDataccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Myshop.core.Models;
+using Myshop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyshopDataccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+            {
+
+            }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Productcategory> Productcategories { get; set; }
+
+    }
+}
---
+++ myShop/MyshopDataccess.SQL/Migrations/202110271022038_initial.cs
@@ -0,0 +1,42 @@
+﻿namespace MyshopDataccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Productcategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Category = c.String(),
+                        CreatAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Category = c.String(),
+                        Image = c.String(),
+                        CreatAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.Productcategories");
+        }
+    }
+}
