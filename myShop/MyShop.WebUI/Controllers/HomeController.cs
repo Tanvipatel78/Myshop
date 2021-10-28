4b44b7002535cd8270ac0c2ccad10ae12628959c
Tanvipatel78 <80584956+Tanvipatel78@users.noreply.github.com>
2021-10-28 10:39:38 +0530

    Product Listning

--- myShop/MyShop.WebUI/Controllers/HomeController.cs
+++ myShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Myshop.core.Contracts;
+using Myshop.core.Models;
+using Myshop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +11,32 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> Context;
+        IRepository<Productcategory> productcategories;
+
+        public HomeController(IRepository<Product> productContext, IRepository<Productcategory> productCategoryContext)
+        {
+            Context = productContext;
+            productcategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = Context.Collection().ToList();
+            return View(products);
         }
 
+        public ActionResult Details(string Id)
+        {
+            Product product = Context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
