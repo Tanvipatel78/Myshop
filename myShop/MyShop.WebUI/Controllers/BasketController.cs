ba58af347adcc944f6987784f099e18565861264
Tanvipatel78 <80584956+Tanvipatel78@users.noreply.github.com>
2021-10-29 13:05:15 +0530

    serviceUI

---
+++ myShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,25 @@
+﻿using Myshop.core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService basketservice;
+
+        public BasketController(IBasketService BasketService)
+        {
+            this.basketservice = BasketService;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
